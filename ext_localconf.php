ac5e48eb98c3fa5d252f2fd0c87fbde5ffc2e59b
Patrick Schriner <patrick.schriner@diemedialen.de>
2024-10-15 18:49:10 +0200

    Move page tsconfig to proper place

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,4 +7,9 @@ $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['dierenkliniektilburgoudnoord'] = 
 /***************
  * PageTS
  */
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig("@import 'EXT:dierenkliniektilburgoudnoord_sitepackage/Configuration/TsConfig/Page/All.tsconfig'");
+
+$versionInformation = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Information\Typo3Version::class);
+// Only include page.tsconfig if TYPO3 version is below 12 so that it is not imported twice.
+if ($versionInformation->getMajorVersion() < 12) {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig("@import 'EXT:dierenkliniektilburgoudnoord_sitepackage/Configuration/page.tsconfig'");
+}
